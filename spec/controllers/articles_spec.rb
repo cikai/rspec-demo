822489a5418693caa0c5792b650156f38ccc1c26
cikai <cikai0923@gmail.com>
2016-06-24 10:13:31 +0800

    testing model and controller

--- spec/controllers/articles_spec.rb
+++ spec/controllers/articles_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe ArticlesController, :type => :controller do
       expect(response).to have_http_status(200)
     end
 	end
+
+	describe 'GET #new' do
+    it 'create new blog # normally' do
+      article = Article.create!(:title => 'testing', :text => 'testing msg')
+      get :new
+      assert_select 'f.submit' do
+        assert_select 'input[name=?]', 'title'
+        assert_select 'textarea[name=?]', 'text'
+        assert_select 'input[type=?]', 'submit'
+      end
+    end
+
+    it 'create new blog # FactoryGirl' do
+      article = FactoryGirl.create(:title => 'FactoryGirl testing', :text => 'testing msg')
+      visit '/new'
+      fill_in 'article[title]', :with => 'FactoryGirl testing'
+      fill_in 'article[text]', :with => 'testing msg'
+      click_button 'commit'
+
+      expect(page).to have_selector("h1", :text => "testing msg")
+    end
+	end
 end
